c7299900ddef95feee6c36ffb0abd1173eeb8577
lukas <lukasbelay87@gmail.com>
2022-08-26 22:59:07 -0700

    setup threed background

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Renderer } from "expo-three";
+import { Renderer, TextureLoader } from "expo-three";
 import { View } from "react-native";
 import { GLView } from "expo-gl";
 import {
@@ -12,24 +12,35 @@ import {
 
 const App = () => {
   const onContextCreate = async (gl) => {
+    const Bruh = new TextureLoader().load(require("./assets/space.jpg"));
     const scene = new Scene();
+    scene.background = Bruh;
     const camera = new PerspectiveCamera(
       75,
       gl.drawingBufferWidth / gl.drawingBufferHeight,
       0.1,
       1000
     );
-    camera.position.z = 2;
+    camera.position.z = 5;
+    camera.position.x = 0;
+    camera.position.y = 0;
     gl.canvas = {
-      width: gl.drawingBufferWidth,
-      height: gl.drawingBufferHeight,
+      width: gl.width,
+      height: gl.height,
     };
     const renderer = new Renderer({ gl });
-    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+    renderer.setSize(gl.Width, gl.Height);
+    const pointLight = new THREE.AmbientLight(0xffffff);
+    pointLight.position.set(20, 20, 20);
+
+    scene.add(pointLight);
 
     const geometry = new BoxBufferGeometry(1, 1, 1);
-    const material = new MeshBasicMaterial({ color: 0xff6347 });
+    const material = new MeshBasicMaterial({ color: 0xff6447 });
     const cube = new Mesh(geometry, material);
+    cube.position.y = 0;
+    cube.position.x = 0;
+
     scene.add(cube);
 
     const render = () => {
@@ -45,7 +56,7 @@ const App = () => {
     <View>
       <GLView
         onContextCreate={onContextCreate}
-        style={{ width: 300, height: 300 }}
+        style={{ width: 400, height: "100%" }}
       />
     </View>
   );
